774886c34a4ac157954062cbbfc2790a929f125c
Kyle Sonaty <kyle.sonaty@gmail.com>
2014-12-02 20:19:38 -0500

    Initial Commit

---
+++ src/NLogCassandra/CassandraTarget.fs
@@ -0,0 +1,31 @@
+﻿namespace NLogCassandra
+
+open Cassandra
+open NLog
+open NLog.Targets
+open System.Collections.Generic
+
+type CassandraTarget(nodes:string[], keyspace:string, replication:int, columnFamily:string) = 
+    inherit TargetWithLayout()
+    let mutable _nodes = nodes
+    let cluster = Cluster.Builder().AddContactPoints(nodes).Build()
+    let session = cluster.Connect()
+    do 
+        let q = Queries.CreateKeyspace(keyspace, replication)
+        session.Execute(q) |> ignore
+        session.Execute(Queries.CreateTable(keyspace, columnFamily)) |> ignore
+   
+    let statement = session.Prepare(Queries.Insert(keyspace, columnFamily))
+
+    override this.Write(logEvent:LogEventInfo) =
+        let message = this.Layout.Render(logEvent)
+        let stackTrace = match logEvent.HasStackTrace with
+                            | true -> logEvent.StackTrace.ToString()
+                            | _ -> ""
+        let boundedStatement = 
+            statement.Bind(logEvent.SequenceID, logEvent.TimeStamp, logEvent.Level.ToString(), 
+                logEvent.FormattedMessage, stackTrace, logEvent.LoggerName)
+        printfn "Writing log: %A" logEvent.FormattedMessage
+        session.Execute(boundedStatement) |> ignore
+        printfn "Wrote log: %A" logEvent.FormattedMessage
+       
---
+++ src/NLogCassandra/Queries.fs
@@ -0,0 +1,23 @@
+﻿namespace NLogCassandra
+
+open NLog
+open System 
+
+module Queries =
+
+    let CreateKeyspace(keyspace, replication) = 
+        String.Format("CREATE KEYSPACE IF NOT EXISTS \"{0}\" WITH REPLICATION = {{ 'class': 'SimpleStrategy', 'replication_factor' : '{1}'}};", keyspace, replication)
+
+    let CreateTable(keyspace, columnFamily) =
+        String.Format("CREATE TABLE IF NOT EXISTS \"{0}\".\"{1}\" 
+            (Id timeuuid PRIMARY KEY,
+            SequenceId int,
+            Timestamp timestamp,
+            Level text,
+            Message text,
+            StackTrace text,
+            Logger text);", keyspace, columnFamily)
+
+    let Insert(keyspace, columnFamily) = 
+        String.Format("INSERT INTO \"{0}\".\"{1}\" (Id, SequenceId, Timestamp, Level, Message, StackTrace, Logger)
+        VALUES (now(),?,?,?,?,?,?)", keyspace, columnFamily)
