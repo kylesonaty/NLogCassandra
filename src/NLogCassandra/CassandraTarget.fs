c0b6e11dd9f3c28c94aabab7e55c7be01faa7db7
Kyle Sonaty <kyle.sonaty@gmail.com>
2014-12-16 15:25:08 -0500

    Refresh schema and create column family statement.

--- src/NLogCassandra/CassandraTarget.fs
+++ src/NLogCassandra/CassandraTarget.fs
@@ -17,15 +17,16 @@ type CassandraTarget(nodes:string[], keyspace:string, replication:int, columnFam
     let mutable _columnFamily = columnFamily
     let mutable _nodes = nodes
 
-    let cluster = new Lazy<Cluster>(fun _ -> Cluster.Builder().AddContactPoints(_nodes).Build())
-    let session = new Lazy<ISession>(fun _ -> cluster.Value.Connect())
+    let rep = dict [ ("class", "SimpleStrategy"); ("replication_factor", _replication.ToString()) ]
+    let cluster = new Lazy<Cluster>(fun _ -> Cluster.Builder().WithDefaultKeyspace(_keyspace).AddContactPoints(_nodes).Build())
+    let session = new Lazy<ISession>(fun _ -> cluster.Value.ConnectAndCreateDefaultKeyspaceIfNotExists(new Dictionary<string, string>(rep)))
     let statement = new Lazy<PreparedStatement>(fun _ -> session.Value.Prepare(Queries.Insert(_keyspace, _columnFamily)))
 
     let init = fun _ -> 
-        let q = Queries.CreateKeyspace(_keyspace, _replication)
-        session.Value.Execute(q) |> ignore
-        session.Value.Execute(Queries.CreateTable(_keyspace, _columnFamily)) |> ignore
-        _initialized <- true    
+        let createColumnFamilyStatement = (new SimpleStatement(Queries.CreateTable(_keyspace, _columnFamily))).SetConsistencyLevel(new Nullable<ConsistencyLevel>(ConsistencyLevel.All))
+        let createResult = session.Value.Execute(createColumnFamilyStatement)
+        cluster.Value.RefreshSchema() |> ignore
+        _initialized <- true
 
     member this.Nodes  with get () = _nodes and set (value) = _nodes <- value
 
@@ -37,11 +38,11 @@ type CassandraTarget(nodes:string[], keyspace:string, replication:int, columnFam
             this.Nodes <- value.Split(',') |> Array.map(fun h -> h.Trim())
 
     [<AdvancedAttribute>]
-    member this.Keyspace with get() = _keyspace and set(value) = _keyspace <- value
+    member this.Keyspace with get() = _keyspace and set(value:string) = _keyspace <- value.Trim()
     [<AdvancedAttribute>]
-    member this.Replication with get() = _replication and set(value) = _replication <- value
+    member this.Replication with get() = _replication and set(value:int) = _replication <- value
     [<AdvancedAttribute>]
-    member this.ColumnFamily with get() = _columnFamily and set(value) = _columnFamily <- value
+    member this.ColumnFamily with get() = _columnFamily and set(value:string) = _columnFamily <- value.Trim()
 
     override this.Write(logEvent:LogEventInfo) =
         if (not _initialized) then
--- src/NLogCassandra/Queries.fs
+++ src/NLogCassandra/Queries.fs
@@ -5,9 +5,6 @@ open System
 
 module Queries =
 
-    let CreateKeyspace(keyspace, replication) = 
-        String.Format("CREATE KEYSPACE IF NOT EXISTS \"{0}\" WITH REPLICATION = {{ 'class': 'SimpleStrategy', 'replication_factor' : '{1}'}};", keyspace, replication)
-
     let CreateTable(keyspace, columnFamily) =
         String.Format("CREATE TABLE IF NOT EXISTS \"{0}\".\"{1}\" 
             (Id timeuuid PRIMARY KEY,
@@ -20,4 +17,4 @@ module Queries =
 
     let Insert(keyspace, columnFamily) = 
         String.Format("INSERT INTO \"{0}\".\"{1}\" (Id, SequenceId, Timestamp, Level, Message, StackTrace, Logger)
-        VALUES (now(),?,?,?,?,?,?)", keyspace, columnFamily)
+        VALUES (now(),?,?,?,?,?,?);", keyspace, columnFamily)
